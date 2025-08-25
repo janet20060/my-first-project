7cda78b4d36155e8284985724c837b4fb96259d5
janet katumpa <rachelmutale06@gmail.com>
2025-08-24 22:42:36 -0700

    added c program and output file

---
+++ age.c
@@ -0,0 +1,21 @@
+// main.c
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    // Prompt user to enter their age
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    // Categorize age using if-else if-else statements
+    if (age < 13) {
+        printf("You are a child!\n");
+    } else if (age >= 13 && age <= 19) {
+        printf("You are a teenager!\n");
+    } else {
+        printf("You are an adult!\n");
+    }
+
+    return 0;
+}
